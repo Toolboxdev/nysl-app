309b0ba402f3110f7ecbbe3747e9fecc4dd8598c
Toolboxdev <48371387+Toolboxdev@users.noreply.github.com>
2020-06-08 20:40:01 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*document.getElementById("mapa").style.display="none";*/
+
+
+/*$('.mapa').hide();*/
+/* landscape muestra mapa)*/
+var lugar = $('.lugar'); 
+
+/* Menu Navegacion */
+function rolePage(newSection){
+	app.section = newSection;
+}
+
+
+$(document).on("click", function(e){
+		$("#menu").hide();
+});
+
+
+document.getElementById("home").style.display="display"
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+				section: "home",
+			}
+				
+		});
+
+
+
+
